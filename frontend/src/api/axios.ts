f53ca827a43022aef7ee8df9d0dc07082f5dca07
yallz <admin@zerity.ru>
2026-01-03 22:49:41 +0300

    changed axios baseuri port

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://subsora.mywire.org:48800/api/v1',
+  baseURL: 'https://subsora.mywire.org:48080/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
